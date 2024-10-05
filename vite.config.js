a01dbcd81daa05a06ba365f43e375b3c58ded2b3
Dmitry160691 <160691@mail.ru>
2024-10-06 02:28:13 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  root: resolve(__dirname, './'),
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
+    emptyOutDir: true,
+    outDir: resolve(__dirname, 'dist'),
   },
   css: {
     postcss: "./postcss.config.js",
